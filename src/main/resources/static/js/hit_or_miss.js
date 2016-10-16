eec78da143e79da640619923f0d564f91e8481e7
Amith Nambiar <amith.nmbr@gmail.com>
2016-10-16 19:34:45 +0530

    some final edits

--- src/main/resources/static/js/hit_or_miss.js
+++ src/main/resources/static/js/hit_or_miss.js
@@ -1,14 +1,8 @@
-//See: https://github.com/pablojim/highcharts-ng
 var myapp = angular.module('myapp', []);
 
 myapp.controller('myctrl', ['$scope','$http', function($scope, $http)  {
      $scope.ids = [];
      $scope.entries = [];
-
-     for(id=0; id<10; id++) {
-        $scope.ids.push(id);
-     }
-
      $scope.selected_id = 1;
 
      $scope.fetch = function(selected_id) {
@@ -17,6 +11,8 @@ myapp.controller('myctrl', ['$scope','$http', function($scope, $http)  {
                 if(response.status == 200) {
                     console.log("Response " + JSON.stringify(response.data));
                     $scope.entries.unshift(response.data);
+            } else {
+                //TODO: Report error to the user
             }
          });
      }
