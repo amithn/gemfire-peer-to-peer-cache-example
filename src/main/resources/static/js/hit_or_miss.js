e075d6c37196adc8c07a2d3c4a1e4322c6827557
Amith Nambiar <amith.nmbr@gmail.com>
2016-10-17 11:27:04 +0530

    fixing a bug in the UI

--- src/main/resources/static/js/hit_or_miss.js
+++ src/main/resources/static/js/hit_or_miss.js
@@ -3,6 +3,11 @@ var myapp = angular.module('myapp', []);
 myapp.controller('myctrl', ['$scope','$http', function($scope, $http)  {
      $scope.ids = [];
      $scope.entries = [];
+
+     for(id=0; id<10; id++) {
+        $scope.ids.push(id);
+     }
+
      $scope.selected_id = 1;
 
      $scope.fetch = function(selected_id) {
